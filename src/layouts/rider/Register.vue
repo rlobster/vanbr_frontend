9c10830c57f15a6b52467d8d9dc49c79c629cf28
rlobster <s.ironthrone@gmail.com>
2018-07-04 19:46:49 +0530

    fixed Register axios error

--- src/layouts/rider/Register.vue
+++ src/layouts/rider/Register.vue
@@ -75,7 +75,7 @@
             dob: this.dob,
             gender: this.gender,
           };
-          const response = await axios.post('http://vanbr.ca/api/rider/signup', data);
+          const response = await this.axios.post('http://vanbr.ca/api/rider/signup', data);
           localStorage.setItem('token', response.data.token);
           this.axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
           this.$router.push(Routes.Booking);
