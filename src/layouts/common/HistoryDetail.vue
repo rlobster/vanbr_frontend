9152555d3c076164128ef5de9e8db7c085d03ab0
rlobster <s.ironthrone@gmail.com>
2018-09-03 14:23:38 -0400

    Service chage displayed

--- src/layouts/common/HistoryDetail.vue
+++ src/layouts/common/HistoryDetail.vue
@@ -97,7 +97,7 @@
                   <div><strong>${{cost_meta_data.vanbr_charges}}</strong></div>
                 </div>
               </div>
-              <div v-if="ride.ride_status === 3">
+              <div>
                 <div class="d-flex justify-content-between">
                   <div>Service charge:</div>
                   <div><strong>${{cost_meta_data.service_charges}}</strong></div>
