c2cc47d0b40a13d984d3b7866e683f37fbf9eb9b
rlobster <s.ironthrone@gmail.com>
2018-09-03 22:34:21 -0400

    Home page updated

--- src/layouts/common/Home.vue
+++ src/layouts/common/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home container main-app-section-sm">
-    <div class="title text-center">Home welcome</div>
+  <div class="home main-app-section-sm">
     <!-- <div class="d-flex justify-content-between main-app-section-sm">
       <router-link :to='Routes.Login'>Go to Login</router-link>
       <router-link :to='Routes.Register'>Go to Register</router-link>
@@ -8,20 +7,101 @@
       <router-link :to='Routes.Ride'>View Ride Info</router-link>
       <router-link :to='Routes.Payment'>Pay a Ride</router-link>
     </div> -->
+    <header class="business-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12" style="height:400px">
+            <h1 class="display-3 text-center text-white mt-4">Business Name or Tagline</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+        
+        <div class="col-sm-12">
+          <h2 class="mt-4">What We Do</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
+          <p>
+            <a class="btn btn-primary btn-lg" href="#">Call to Action &raquo;</a>
+          </p>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+
+      <div class="row mt-4 pt-4">
+
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="http://placehold.it/300x200" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="http://placehold.it/300x200" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 my-4">
+          <div class="card">
+            <img class="card-img-top" src="http://placehold.it/300x200" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            </div>
+            <div class="card-footer">
+              <a href="#" class="btn btn-primary">Find Out More!</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    <!-- /.row -->
+    </div>
+    
+    <!-- Footer -->
+    <footer class="py-3 bg-dark mt-5">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
   </div>
 </template>
 
 <script>
-  import Routes from '@/router/routes';
+import Routes from '@/router/routes';
 
-  export default {
-    name: 'home',
-    data() {
-      return {
-        Routes,
-      };
-    },
-  };
+export default {
+  name: 'home',
+  data() {
+    return {
+      Routes,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
