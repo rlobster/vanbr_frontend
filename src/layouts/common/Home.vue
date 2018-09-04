ce7e1127615455fd8791241759a890e7e8063f9b
rlobster <s.ironthrone@gmail.com>
2018-09-04 09:33:37 -0400

    Update Home.vue

--- src/layouts/common/Home.vue
+++ src/layouts/common/Home.vue
@@ -8,12 +8,8 @@
       <router-link :to='Routes.Payment'>Pay a Ride</router-link>
     </div> -->
     <header class="business-header">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12" style="height:400px">
-            <h1 class="display-3 text-center text-white mt-4">Business Name or Tagline</h1>
-          </div>
-        </div>
+      <div style="background:#aaa">
+        <h1 class="display-3 text-center text-white mt-5 mb-5">Welcome to Vanbr.</h1>
       </div>
     </header>
 
@@ -23,7 +19,7 @@
       <div class="row">
         
         <div class="col-sm-12">
-          <h2 class="mt-4">What We Do</h2>
+          <h2 class="mt-4 mb-2">What We Do</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
           <p>
@@ -37,7 +33,7 @@
 
       <div class="row mt-4 pt-4">
 
-        <div class="col-sm-4 my-4">
+        <div class="col-sm-6 my-4">
           <div class="card">
             <img class="card-img-top" src="http://placehold.it/300x200" alt="">
             <div class="card-body">
@@ -50,20 +46,7 @@
           </div>
         </div>
 
-        <div class="col-sm-4 my-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-4 my-4">
+        <div class="col-sm-6 my-4">
           <div class="card">
             <img class="card-img-top" src="http://placehold.it/300x200" alt="">
             <div class="card-body">
