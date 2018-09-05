098ff1f956470dd1694a84dd62453f5d4e4e3005
rlobster <s.ironthrone@gmail.com>
2018-09-05 19:44:23 -0400

    Stripe publishable key updated in constant

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const AppURL = 'https://vanbr.ca/api';
-// const STRIPE_PUBLISHABLE_KEY = 'pk_live_sdXOBGnhdpvzV9QOpTWnJgij';
+const STRIPE_PUBLISHABLE_KEY = 'pk_live_sdXOBGnhdpvzV9QOpTWnJgij';
 // const AppURL = 'http://vanbr.local:8021/api';
-const STRIPE_PUBLISHABLE_KEY = 'pk_test_8wgmvT01TU27qZFaMbAny3UF';
+// const STRIPE_PUBLISHABLE_KEY = 'pk_test_8wgmvT01TU27qZFaMbAny3UF';
 
 export { AppURL, STRIPE_PUBLISHABLE_KEY };
